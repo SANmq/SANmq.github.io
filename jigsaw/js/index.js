42a570691a19dc609d8cad9846a88ed6fbc6d349
SANmq <739699025@qq.com>
2020-05-12 23:00:34 +0800

    完善细节
    增加可扩展格式，之后需要处理格子间的边框问题。可以用双重嵌套或者表格的方式完成，表格可能会影响效率，所以不考虑

---
+++ jigsaw/js/index.js
@@ -0,0 +1,61 @@
+Jigsaw.prototype = {
+
+    init: function () {
+        this.initData();
+        this.render();
+        this.itemObj();
+    },
+
+    initData: function () {
+        this.ul = this.el.getElementsByTagName("ul")[0];
+        this.ulwidth = parseInt(window.getComputedStyle(this.ul, null).width);
+        this.liwidth = (this.ulwidth) / this.boder;
+    },
+
+    itemObj() {
+        this.item = new Array()
+        for (let i = 0; i < this.boder ** 2; i++) {
+            this.item.push({
+                no: this.ul.childNodes[i],
+                now: i,
+                top:1,
+                left:2,
+                bottom:3,
+                right:4
+            })
+        }
+        console.log(this.item)
+    },
+
+    render: function () {
+        let temp = ""
+        for (let i = 0; i < this.boder ** 2; i++) {
+            let step = 1 / (this.boder - 1) * 100
+            let x = step * parseInt(i % this.boder);
+            let y = step * parseInt(i / this.boder);
+            if (i == this.boder ** 2 - 1) {
+                temp += `<li class="item${i}" style="width:${this.liwidth}px;height:${this.liwidth}px;
+                background-image:none"></li>`
+            } else {
+                temp += `<li class="item${i}" style="width:${this.liwidth}px;height:${this.liwidth}px;
+                background-size: ${this.boder}00% ${this.boder}00%;background-position:${x}% ${y}%;"></li>`
+            }
+        }
+        this.ul.style.height = this.ulwidth + 'px';
+        this.ul.innerHTML = temp;
+    }
+}
+
+//  九宫格拼图
+function Jigsaw(el, img, boder) {
+    // 游戏区域
+    this.el = el
+    // 记录所用图片
+    this.img = img;
+    // 记录格子数量
+    this.boder = boder;
+    this.init();
+}
+
+var el = document.getElementById('pintu');
+var game = new Jigsaw(el, "img/1.jpg", 3)
