3ec2f58b547fac60668b11c933d6793687b9eaa7
SANmq <50514663+SANmq@users.noreply.github.com>
2020-05-13 13:30:52 +0800

    浮动改为定位

--- jigsaw/js/index.js
+++ jigsaw/js/index.js
@@ -13,15 +13,20 @@ Jigsaw.prototype = {
     },
 
     itemObj() {
-        this.item = new Array()
+        this.item = []
         for (let i = 0; i < this.boder ** 2; i++) {
+            let step = 1 / (this.boder - 1) * 100
+            let x = step * parseInt(i % this.boder);
+            let y = step * parseInt(i / this.boder);
+            let left = parseInt(i % this.boder) * this.liwidth;
+            let top = parseInt(i / this.boder) * this.liwidth;
             this.item.push({
-                no: this.ul.childNodes[i],
+                no: null,
                 now: i,
-                top:1,
-                left:2,
-                bottom:3,
-                right:4
+                x: x,
+                y: y,
+                left: left,
+                top: top
             })
         }
         console.log(this.item)
@@ -33,12 +38,14 @@ Jigsaw.prototype = {
             let step = 1 / (this.boder - 1) * 100
             let x = step * parseInt(i % this.boder);
             let y = step * parseInt(i / this.boder);
+            let left = parseInt(i % this.boder) * this.liwidth;
+            let top = parseInt(i / this.boder) * this.liwidth;
             if (i == this.boder ** 2 - 1) {
                 temp += `<li class="item${i}" style="width:${this.liwidth}px;height:${this.liwidth}px;
-                background-image:none"></li>`
+                background-image:none;left:${left}px;top:${top}px"></li>`
             } else {
                 temp += `<li class="item${i}" style="width:${this.liwidth}px;height:${this.liwidth}px;
-                background-size: ${this.boder}00% ${this.boder}00%;background-position:${x}% ${y}%;"></li>`
+                background-size: ${this.boder}00% ${this.boder}00%;background-position:${x}% ${y}%;left:${left}px;top:${top}px"></li>`
             }
         }
         this.ul.style.height = this.ulwidth + 'px';
